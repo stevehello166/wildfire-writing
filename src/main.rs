885c1372e08b6c70bf2c2155f405c9a9d19179e6
Joseph <stevehello166@gmail.com>
2023-08-14 09:55:43 -0700

    Started actually using fltk-decl

--- src/main.rs
+++ src/main.rs
@@ -19,66 +19,24 @@ Nadichamp
 
 
 // the Import block
-pub mod menu_manipulation;
-use menu_manipulation::*;
-pub mod file_manipulation;
+mod file_manipulation;
+mod theme_and_decoration;
 use file_manipulation::*;
-pub mod theme_and_decoration;
+use theme_and_decoration::*;
 
-use fltk_decl::*;
+use fltk::{prelude::*, *, enums::*};
+use fltk_decl::{DeclarativeApp,Widget};
 use std::env;
-use fltk::{menu, menu::*, app, prelude::MenuExt};
 
 pub const OPERATING_SYSTEM: &str = env::consts::OS;
 pub const APP_NAME: &str = "Wildfire Write";
 
-
-pub struct Configuration {
-    pub screen_dimension_w: u32,
-    pub screen_dimension_h: u32,
-    pub menu_bar_height: u32,
-    pub side_bar_width: u32,
-    pub side_bar_button_height: u32,
-}
-
-
-
 fn main() {
-    println!("{}", OPERATING_SYSTEM);
-    // portng to fltk decl something i should've done from the start
-    DeclarativeApp::new_json(200, 300, "wildfire_write", "resources/gui/gui.json").run(|_win|
-    {
-        if let Some(mut menu_bar) = app::widget_from_id::<menu::Choice>("menu_bar") {
-            menu_bar.add_choice("File|Edit|Preferences|License|File/Open");
-        }
-
-    }).unwrap();
-
-    // all the widgets for the window
-    /*let mut wind = Window::new(720, 1280, 1600, 900, APP_NAME);
-    let mut menubar = SysMenuBar::new(0,0,1600, 25, APP_NAME);
-
-    // open file
-    let mut file_str:String = String::new();
-    // menu bar stuff
-    
-    let app_clone = a.clone();
-    add_to_menubar(menubar.clone(),app_clone);
-
-    menubar.add(
-        "File/Open\t",
-        Shortcut::None,
-        menu::MenuFlag::Normal,
-        move |_|{
-            file_str = open_file();
-            println!("{}", file_str);
-        },
-    );
-
-    // turn this into a code module
-
-    module_ui(a.clone());
-    
-    */
-    //Runs at application close
+    DeclarativeApp::new_json(1280, 720, "MyApp", "resources/gui/gui.json").run(|_win| 
+        {
+            //if let Some(mut choice) = app::widget_from_id::<menu::Choice>("choice")
+            if let Some (mut wind) = app::widget_from_id::<window::DoubleWindow>("wind"){
+            wind.set_color(Color::Black)
+            }
+        }).unwrap();
 }
